757fbdd6ea779856f4f737080a27eb78554032c8
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2024-06-19 16:26:36 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 let listMobile = document.getElementById('listMobile')
 let navMobile = document.getElementById('nav_mobile')
+const body = document.body;
 function menuShow() {
     if(!listMobile.classList.contains('active')) {
         listMobile.classList.add('active')
         navMobile.classList.add('active')
+        body.classList.add('no-scroll')
     } else {
         listMobile.classList.remove('active')
         navMobile.classList.remove('active')
+        body.classList.remove('no-scroll')
     }
 }
