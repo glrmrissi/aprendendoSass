49f63a41928e04d0915b942bf61f6f0426dc52f8
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2024-06-20 03:32:47 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 let listMobile = document.getElementById('listMobile')
 let navMobile = document.getElementById('nav_mobile')
+let imgHamburguer = document.getElementById('hamburguer-img')
 const body = document.body;
 function menuShow() {
     if(!listMobile.classList.contains('active')) {
         listMobile.classList.add('active')
         navMobile.classList.add('active')
+        imgHamburguer.src = '../../../assets/imgs/ham2.svg'
         body.classList.add('no-scroll')
     } else {
         listMobile.classList.remove('active')
         navMobile.classList.remove('active')
+        imgHamburguer.src = '../../../assets/imgs/ham.svg'
         body.classList.remove('no-scroll')
     }
 }
