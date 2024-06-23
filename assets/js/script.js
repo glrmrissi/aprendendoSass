1185f538adeda9b9618c626e54710ad54973840c
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2024-06-23 03:54:18 -0300

    Btn-top adicionado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,39 @@
-let listMobile = document.getElementById('listMobile')
-let navMobile = document.getElementById('nav_mobile')
-let imgHamburguer = document.getElementById('hamburguer-img')
 const body = document.body;
+
+const HAMBURGER_ICON_ACTIVE = 'assets/imgs/ham2.svg';
+const HAMBURGER_ICON_INACTIVE = 'assets/imgs/ham.svg';
+
 function menuShow() {
-    if(!listMobile.classList.contains('active')) {
-        listMobile.classList.add('active')
-        navMobile.classList.add('active')
-        imgHamburguer.src = 'assets/imgs/ham2.svg'
-        body.classList.add('no-scroll')
-    } else {
-        listMobile.classList.remove('active')
-        navMobile.classList.remove('active')
-        imgHamburguer.src = 'assets/imgs/ham.svg'
-        body.classList.remove('no-scroll')
-    }
+    const hamburgerIcon = mobile.hamburgerIcon();
+    const mobileNav = mobile.mobileNav();
+    const mobileMenuList = mobile.mobileMenuList();
+    
+    const isActive = mobileMenuList.classList.toggle('active');
+    mobileNav.classList.toggle('active', isActive);
+    hamburgerIcon.src = isActive ? HAMBURGER_ICON_ACTIVE : HAMBURGER_ICON_INACTIVE;
+    body.classList.toggle('no-scroll', isActive);
 }
+
+const mobile = {
+    hamburgerIcon: () => document.getElementById('hamburguer-img'),
+    mobileNav: () => document.getElementById('nav_mobile'),
+    mobileMenuList: () => document.getElementById('listMobile')
+}
+
+let btn = document.getElementById('btnTop')
+
+btn.addEventListener("click", function(){
+    window.scrollTo(0, 0)
+})
+
+function ocultar() {
+  if (window.scrollY < 10) {
+      btn.style.visibility = "hidden";
+      btn.style.opacity = "0";
+  } else {
+      btn.style.visibility = "visible";
+      btn.style.opacity = "1";
+  }
+}
+
+window.addEventListener("scroll", ocultar)
